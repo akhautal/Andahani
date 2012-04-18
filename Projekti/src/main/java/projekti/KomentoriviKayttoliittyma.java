c3276c88e060ef078e0046bd64e3df740f2b5a2b
Daria Antonova <daria.antonova@helsinki.fi>
2012-04-19 00:41:49 +0300

    refaktorointi

--- Projekti/src/main/java/projekti/KomentoriviKayttoliittyma.java
+++ Projekti/src/main/java/projekti/KomentoriviKayttoliittyma.java
@@ -4,7 +4,6 @@
  */
 package projekti;
 
-import java.util.ArrayList;
 /**
  *
  * @author hanna
@@ -12,16 +11,25 @@ import java.util.ArrayList;
 public class KomentoriviKayttoliittyma implements KayttoliittymaRajapinta{
     private IOrajapinta io;
     private TiedostonkasittelijaRajapinta tiedostonKasittelija;
-    private Bibtallentaja bibtallentaja;
+    private Komentotehdas toiminnot;
     
     public KomentoriviKayttoliittyma(IOrajapinta io, TiedostonkasittelijaRajapinta tallentaja) { 
         this.io = io;
         this.tiedostonKasittelija = tallentaja;
-        bibtallentaja = new Bibtallentaja();
+        toiminnot = new Komentotehdas(io, tallentaja);            
     }
     
     public KomentoriviKayttoliittyma(IOrajapinta io) { 
         this.io = io;
+    }
+ 
+    public void kaynnista(){
+        naytaOhjeet();
+        while(true){
+            String syote = io.lue();
+            toiminnot.hae(syote).suorita();
+            naytaOhjeet();
+        }
     }
 
     public void naytaOhjeet() {
@@ -31,32 +39,6 @@ public class KomentoriviKayttoliittyma implements KayttoliittymaRajapinta{
         io.tulosta("\"bib\" tulostaa kaikki viitteet bibtex-muodossa.");
         io.tulosta("\n");
     }
-    
-    public void kaynnista(){
-        naytaOhjeet();
-        while(true){
-            String syote = io.lue();
-            if(syote == null){
-                naytaOhjeet();
-                continue;
-            }
-            if(syote.equalsIgnoreCase("lopeta")){
-                break;
-            }
-            else if(syote.equalsIgnoreCase("lista")){
-                listaa();
-            }
-            else if(syote.equalsIgnoreCase("lisaa")){
-                lisaa();
-            }
-            else if(syote.equalsIgnoreCase("bib")){
-                bibTulostus();
-            }
-            naytaOhjeet();
-            
-        }
-    }
-
 
     public Viite annaViite() {
 //        String syote;
@@ -90,160 +72,5 @@ public class KomentoriviKayttoliittyma implements KayttoliittymaRajapinta{
 //        return uusi;
         return null;
     }
-    
-    
-    private void lisaa(){
-        Viite uusi = kysySyotetta();
-        
-        io.tulosta("lisätään viite järjestelmään.");
-        tiedostonKasittelija.tallenna(uusi);
-        
-        
-    }
-    
-    private void listaa(){        
-        ArrayList<Viite> viitteet = tiedostonKasittelija.lueViitteet();
-        
-        if(viitteet != null) {
-            tulostaViitteet(viitteet);
-        }
-        else {
-            io.tulosta("Viitteita ei ole tai tiedosto ei ole olemassa.\n");
-        } 
-    }
-    
-    
-    private Viite kysySyotetta(){
-        String[] kentat = {"author", "title", "year", "publisher", "booktitle", "pages", 
-            "journal", "volume", "number",  "address"};
-        String syote;
-        Viite uusi = new Viite();
-        int i = 0;
-        
-        uusi.lisaaTietoa("millainenViite", viitteenLaatu());
-        uusi.lisaaTietoa("label", annaLabel());
-        while(i < kentat.length){
-            io.tulosta(kentat[i] + ":");
-            syote = io.lue();
-            if(syote != null && !syote.equals("")){
-                uusi.lisaaTietoa(kentat[i], syote);
-            }
-            i++;
-        }
-        return uusi;
-    }
-    
-    private String viitteenLaatu(){
-        io.tulosta("Millaisen viitteen haluat lisätä?");
-        io.tulosta("(anna numero)");
-        io.tulosta("1. @inproceedings \n2. @book \n3. @article");
-        String vastaus = io.lue();
-        while(!vastaus.equals("1") && !vastaus.equals("2") && !vastaus.equals("3")){
-            io.tulosta("(anna numero)");
-            io.tulosta("1. @inproceedings \n2. @book \n3. @article");
-            vastaus = io.lue();
-        }
-        if(vastaus.equals("1")){
-            return "@inproceedings";
-        }
-        else if (vastaus.equals("2")){
-            return "@book";
-        }
-        return "@article";
-    }
-    
-    private boolean onkoLabelJoKaytossa(String ehdotettuLabel){
-        ArrayList<Viite> viitteet = tiedostonKasittelija.lueViitteet();
-        
-        if(viitteet == null){
-            return false;
-        }
-        
-        int i = 0;
-        while(i < viitteet.size()){
-            if(viitteet.get(i).getLabel().equalsIgnoreCase(ehdotettuLabel)){
-                return true;
-            }
-            i++;
-        }
-        
-        return false;
-        
-    }
-
-    private String annaLabel() {
-        io.tulosta("Label:");
-        String syote = io.lue();
-        while(onkoLabelJoKaytossa(syote)){
-            io.tulosta("Tämä label on jo käytössä!");
-            io.tulosta("Valitse toinen label.");
-            io.tulosta("Label:");
-            syote = io.lue();
-        }
-        return syote;
-    }
-    
-    private void tulostaViitteet(ArrayList<Viite> viitteet){
-        int i = 0;
-        int j = 0;
-        String[][] tiedot;
-        
-        while(i < viitteet.size()){
-            tiedot = viitteet.get(i).annaTiedot();
-            while(j < tiedot.length){
-                if(!tiedot[j][1].equals("")){
-                    io.tulosta(tiedot[j][0] + " = " + tiedot[j][1]);
-                }
-                j++;
-            }
-            io.tulosta("");
-            j = 0;
-            i++;
-        }
-    }
-
-    private void bibTulostus() {
-        String tiedostonimi = bibTiedostonNimi();
-        
-        ArrayList<Viite> viitteet = tiedostonKasittelija.lueViitteet();
-        int i = 0;
-        while(i < viitteet.size()){
-            bibtallentaja.tallenna(viitteet.get(i));
-            //bibtallentaja.tallenna(viitteet.get(i), tiedostonimi);
-            i++;
-        }
-    }
-    private String bibTiedostonNimi(){
-        io.tulosta("Millä nimellä haluat tallentaa bibtex-tiedoston?");
-        String tiedostonimi = io.lue();
-        while(!onkoKelvollinenBibtexnimi(tiedostonimi)){
-            io.tulosta("Epäkelpo nimi!");
-            io.tulosta("Anna toinen nimi.");
-            tiedostonimi = io.lue();
-        }
-        
-        return tiedostonimi;
-    }
-    
-    private boolean onkoKelvollinenBibtexnimi(String ehdotus){
-        if(ehdotus == null || ehdotus.charAt(0) == '.') {
-            return false;
-        }
-        
-        int i = 0;
-        
-        while (i < ehdotus.length() && ehdotus.charAt(i) != '.'){
-            i++;
-        }
-        
-        if(i == ehdotus.length()){
-            return false;
-        }
-        if(!ehdotus.substring(i+1).equals("bib")){
-            return false;
-        }
-        
-        return true;
-    }
-  
+   
 }
---
+++ Projekti/src/main/java/projekti/toiminnot/Lisaaminen.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekti.toiminnot;
+
+import projekti.Toiminta;
+import java.util.ArrayList;
+import projekti.IOrajapinta;
+import projekti.TiedostonkasittelijaRajapinta;
+import projekti.Viite;
+
+/**
+ *
+ * @author dasha
+ */
+public class Lisaaminen implements Toiminta{
+    private IOrajapinta io;
+    private TiedostonkasittelijaRajapinta tiedostonKasittelija;
+    
+    public Lisaaminen(IOrajapinta io, TiedostonkasittelijaRajapinta tiedostonKasittelija) {
+        this.io = io;
+        this.tiedostonKasittelija = tiedostonKasittelija;
+    }
+ 
+    @Override
+    public void suorita() {
+        Viite uusi = kysySyotetta();
+        
+        io.tulosta("lisätään viite järjestelmään.");
+        tiedostonKasittelija.tallenna(uusi);
+    } 
+    
+    private Viite kysySyotetta(){
+        String[] kentat = {"author", "title", "year", "publisher", "booktitle", "pages", 
+            "journal", "volume", "number",  "address"};
+        String syote;
+        Viite uusi = new Viite();
+        int i = 0;
+        
+        uusi.lisaaTietoa("millainenViite", viitteenLaatu());
+        uusi.lisaaTietoa("label", annaLabel());
+        while(i < kentat.length){
+            io.tulosta(kentat[i] + ":");
+            syote = io.lue();
+            if(syote != null && !syote.equals("")){
+                uusi.lisaaTietoa(kentat[i], syote);
+            }
+            i++;
+        }
+        return uusi;
+    }
+    
+     private String viitteenLaatu(){
+        io.tulosta("Millaisen viitteen haluat lisätä?");
+        io.tulosta("(anna numero)");
+        io.tulosta("1. @inproceedings \n2. @book \n3. @article");
+        String vastaus = io.lue();
+        while(!vastaus.equals("1") && !vastaus.equals("2") && !vastaus.equals("3")){
+            io.tulosta("(anna numero)");
+            io.tulosta("1. @inproceedings \n2. @book \n3. @article");
+            vastaus = io.lue();
+        }
+        if(vastaus.equals("1")){
+            return "@inproceedings";
+        }
+        else if (vastaus.equals("2")){
+            return "@book";
+        }
+        return "@article";
+    }
+     
+    private String annaLabel() {
+        io.tulosta("Label:");
+        String syote = io.lue();
+        while(onkoLabelJoKaytossa(syote)){
+            io.tulosta("Tämä label on jo käytössä!");
+            io.tulosta("Valitse toinen label.");
+            io.tulosta("Label:");
+            syote = io.lue();
+        }
+        return syote;
+    }
+    
+    private boolean onkoLabelJoKaytossa(String ehdotettuLabel){
+        ArrayList<Viite> viitteet = tiedostonKasittelija.lueViitteet();
+        
+        if(viitteet == null){
+            return false;
+        }
+        
+        int i = 0;
+        while(i < viitteet.size()){
+            if(viitteet.get(i).getLabel().equalsIgnoreCase(ehdotettuLabel)){
+                return true;
+            }
+            i++;
+        }
+        
+        return false;
+        
+    }
+}
