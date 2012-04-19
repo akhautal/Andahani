1601a834e7354d2567949f908fafe21b5b6eb712
Daria Antonova <daria.antonova@helsinki.fi>
2012-04-19 15:37:01 +0300

    KomentoTehdas testattu

--- Projekti/src/test/java/projekti/KomentotehdasTest.java
+++ Projekti/src/test/java/projekti/KomentotehdasTest.java
@@ -7,7 +7,7 @@ package projekti;
 import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
-import projekti.toiminnot.Listaaminen;
+import projekti.toiminnot.*;
 
 /**
  *
@@ -25,9 +25,8 @@ public class KomentotehdasTest {
         }
 
         public String lue() {
-            return input.remove(0);
+            throw new UnsupportedOperationException("Not supported yet.");
         }
-        
     };
             
     TiedostonkasittelijaRajapinta tallentajaStub = new TiedostonkasittelijaRajapinta() {
@@ -37,42 +36,8 @@ public class KomentotehdasTest {
         }
 
         public ArrayList<Viite> lueViitteet() {
-            ArrayList<Viite> viitteet = new ArrayList<Viite>();
-            
-            String[][] eka = {{"millainenViite", "2"},
-                                   {"label", "Discworld"},
-                                   {"author", "Pratchett, Terry"},
-                                   {"title",  "Discworld"}};
-
-            String[][] toka = {{"millainenViite", "1"},
-                                    {"label", "Tokalabel"},
-                                    {"author", "Larsson, Stieg"},
-                                    {"title",  "Dragon Tattoo"},
-                                    {"booktitle", "title"},
-                                    {"journal", "-"},
-                                    {"volume", "2"}, 
-                                    {"number", "4"},
-                                    {"year", "1995"},
-                                    {"pages", "600"},
-                                    {"publisher", "Weinerstrom"},
-                                    {"address", "Helsinki, 07078, jokukatu 12c46"}};
-            
-            String[][] kolmas = {{"millainenViite", "1"},
-                                    {"label", "Tokalabel"},
-                                    {"author", "Larsson, Stieg"},
-                                    {"title",  "Dragon Tattoo"},
-                                    {"journal", "-"},
-                                    {"volume", "2"}, 
-                                    {"number", "4"},
-                                    {"year", "1995"},
-                                    {"pages", "600"}};
-            
-            viitteet.add(new Viite(eka));
-            viitteet.add(new Viite(toka));
-            viitteet.add(new Viite(kolmas));
-            return viitteet;
+            throw new UnsupportedOperationException("Not supported yet.");
         }
-        
     };
             
     @Before
@@ -82,27 +47,104 @@ public class KomentotehdasTest {
     }
     
     @Test
-    public void listaTesti() {
-        input.add("lista");
-        Listaaminen expResult = new Listaaminen(ioStub, tallentajaStub);
-        Toiminta lista = komentotehdas.hae("lista");
-        
-        assertEquals(lista, expResult);
-       
+    public void lisaaTesti() {
+        Lisaaminen expResult = new Lisaaminen(ioStub, tallentajaStub);
+        Toiminta result = komentotehdas.hae("lisaa");        
+        assertEquals(result.getClass(), expResult.getClass());
     }
-
-    /**
-     * Test of hae method, of class Komentotehdas.
-     */
+     
     @Test
-    public void testHae() {
-        System.out.println("hae");
-        String operaatio = "";
-        Komentotehdas instance = null;
-        Toiminta expResult = null;
-        Toiminta result = instance.hae(operaatio);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void listaTesti() {
+        Listaaminen expResult = new Listaaminen(ioStub, tallentajaStub);
+        Toiminta result = komentotehdas.hae("lista");        
+        assertEquals(result.getClass(), expResult.getClass());
+    }
+    
+    @Test
+    public void lopetaTesti() {
+        Lopeta expResult = new Lopeta(ioStub);
+        Toiminta result = komentotehdas.hae("lopeta");        
+        assertEquals(result.getClass(), expResult.getClass());
+    }
+     
+    @Test
+    public void bibTesti() {
+        Bib expResult = new Bib(ioStub, tallentajaStub);
+        Toiminta result = komentotehdas.hae("bib");        
+        assertEquals(result.getClass(), result.getClass());
+    }
+      
+    @Test
+    public void tuntematonKomentoTesti() {
+        Tuntematon expResult = new Tuntematon(ioStub);
+        Toiminta result = komentotehdas.hae("unexistentcommand!");        
+        assertEquals(result.getClass(), result.getClass());
+    }
+    
+    @Test
+    public void tyhjaKomentoTesti() {
+        Tuntematon expResult = new Tuntematon(ioStub);
+        Toiminta result = komentotehdas.hae("");        
+        assertEquals(result.getClass(), result.getClass());
     }
 }
+
+
+//ArrayList<String> input; 
+//    
+//    IOrajapinta ioStub = new IOrajapinta() {
+//
+//        public void tulosta(String tuloste) {
+//            throw new UnsupportedOperationException("Not supported yet.");
+//        }
+//
+//        public String lue() {
+//            return input.remove(0);
+//        }
+//        
+//    };
+//            
+//    TiedostonkasittelijaRajapinta tallentajaStub = new TiedostonkasittelijaRajapinta() {
+//
+//        public void tallenna(Viite viite) {
+//            throw new UnsupportedOperationException("Not supported yet.");
+//        }
+//
+//        public ArrayList<Viite> lueViitteet() {
+//            ArrayList<Viite> viitteet = new ArrayList<Viite>();
+//            
+//            String[][] eka = {{"millainenViite", "2"},
+//                                   {"label", "Discworld"},
+//                                   {"author", "Pratchett, Terry"},
+//                                   {"title",  "Discworld"}};
+//
+//            String[][] toka = {{"millainenViite", "1"},
+//                                    {"label", "Tokalabel"},
+//                                    {"author", "Larsson, Stieg"},
+//                                    {"title",  "Dragon Tattoo"},
+//                                    {"booktitle", "title"},
+//                                    {"journal", "-"},
+//                                    {"volume", "2"}, 
+//                                    {"number", "4"},
+//                                    {"year", "1995"},
+//                                    {"pages", "600"},
+//                                    {"publisher", "Weinerstrom"},
+//                                    {"address", "Helsinki, 07078, jokukatu 12c46"}};
+//            
+//            String[][] kolmas = {{"millainenViite", "1"},
+//                                    {"label", "Tokalabel"},
+//                                    {"author", "Larsson, Stieg"},
+//                                    {"title",  "Dragon Tattoo"},
+//                                    {"journal", "-"},
+//                                    {"volume", "2"}, 
+//                                    {"number", "4"},
+//                                    {"year", "1995"},
+//                                    {"pages", "600"}};
+//            
+//            viitteet.add(new Viite(eka));
+//            viitteet.add(new Viite(toka));
+//            viitteet.add(new Viite(kolmas));
+//            return viitteet;
+//        }
+//        
+//    };
